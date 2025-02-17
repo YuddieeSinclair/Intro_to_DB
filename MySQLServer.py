e61c7a3e9af8f21f544ccf0ca97a38539eae3a63
Yuddie <sinclairokoro@gmail.com>
2025-02-17 14:44:49 +0100

    pyhton script to create an SQL database

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,15 +7,14 @@ my_db =  mysql.connector.connect(
 )
 
 try:
-    my_db.is_connected()
-    print("Database 'alx_book_store' created successfully!")
-except:
-    my_db.connector.Error()
-    print("Failed")
-else:
+    if not my_db.is_connected():
+        raise mysql.connector.Error("connection failed!")
+    else:
+        print("Database 'alx_book_store' created successfully!")
     cursor = my_db.cursor()
     my_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
     cursor.execute(my_query)
+except mysql.connector.Error as e: print(f"Error:{e}")
 finally:
     cursor.close()
     my_db.close()
